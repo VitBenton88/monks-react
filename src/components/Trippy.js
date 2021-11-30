54cf2b1a9907f1d6dae00e851b4fc77a69d28612
Vit Benton <Vit@vitbenton.com>
2021-11-29 19:23:01 -0500

    various styling updates

--- src/components/Trippy.js
+++ src/components/Trippy.js
@@ -2,7 +2,7 @@ function Trippy() {
     return ( 
         <div id="trippy-bkg">
             <div class="box yellow"></div>
-            <div class="box cyan"></div>
+            <div class="box turquoise"></div>
             <div class="box black"></div>
             <div class="box pink"></div>
         </div>
