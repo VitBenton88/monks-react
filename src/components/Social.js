025bd07ad6292cb1f30e62fdca9358db971ab29e
Vit Benton <Vit@vitbenton.com>
2022-07-03 13:40:20 -0400

    Added social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,6 +1,21 @@
 function Social() {
     const social_data = [
         {
+          href: "https://open.spotify.com/artist/6bGro9VGKBorkwHf2prS1N",
+          icon: "fa-spotify"
+            
+        },
+        {
+          href: "https://music.apple.com/us/artist/nervous-monks/1482596038",
+          icon: "fa-apple"
+            
+        },
+        {
+          href: "https://music.youtube.com/channel/UChPuHzUFqC0gekc_HbRgluA",
+          icon: "fa-youtube"
+            
+        },
+        {
           href: "https://www.facebook.com/nervousmonks/",
           icon: "fa-facebook"
             
